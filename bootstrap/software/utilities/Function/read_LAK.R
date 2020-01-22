0fdc2443aaad22d9ecf3986f2626878f9825392b
Scruff92 <ruairioggy@hotmail.com>
2020-01-22 18:09:31 +0100

    first and last year of retro runs now automatically set. usebytes=T for some data reading functions

--- bootstrap/software/utilities/Function/read_LAK.R
+++
@@ -1,8 +0,0 @@
-#Function to read Length Age Key (LAK)
-Read.LAK<-function(dir=data.path,read.init.function=FALSE)
-{
-  if (read.init.function) Init.function()
-  file<-file.path(dir,'LAK.out')
-  s<-read.table(file,header=TRUE)
-  data.frame(Species=sp.names[s$Species.no],s)
-}
--- bootstrap/software/utilities/SAN_international_catch.R
+++
@@ -1,9 +0,0 @@
-# script to read total international catches and make a tables for markdown and output folder 
-
-
-a<-read.table(file.path(root,'Data','officialcatches_IV_2017.txt'),stringsAsFactors=F, header=TRUE)
-a[,-1]<-a[,-1]/1000 #change scale to 1000s of tonnes
-a[a==0]<-"-"
-print(knitr::kable(a,align=c('l',rep('r',9)), digits=1))
-
-
